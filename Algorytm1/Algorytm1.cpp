abb82088c096b7c3cc9a769e96db1405cd1ecf1e
MikolajBaran <mikolaj.baran-1@wp.pl>
2022-02-28 15:04:09 +0100

    Started an algorythm

---
+++ Algorytm1/Algorytm1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n = 0;
+	int lp = 0;
+	int p = 0;
+	cout << "Ile liczb pierwszych chcesz wyswietlic";
+	cin >> n;
+	lp = 0;
+	p = 2;
+	for (int d = 2; lp < n;)
+	{
+
+	}
+}
+
+
