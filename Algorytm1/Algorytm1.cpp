8ddb9dd8af054552f423a50f5df90465e488569e
Mikolaj Baran <mikolaj.baran-1@wp.pl>
2022-03-07 15:05:24 +0100

    Did something have to fix it

--- Algorytm1/Algorytm1.cpp
+++ Algorytm1/Algorytm1.cpp
@@ -2,17 +2,35 @@
 using namespace std;
 int main()
 {
-	int n = 0;
-	int lp = 0;
-	int p = 0;
-	cout << "Ile liczb pierwszych chcesz wyswietlic";
+	int n;
+	int lp;
+	int p;
+	int c = 0;
+	cout << "Ile liczb pierwszych chcesz wyswietlic" << endl;
 	cin >> n;
 	lp = 0;
 	p = 2;
-	for (int d = 2; lp < n;)
+	while (lp < n)
 	{
-
+		for (int d = 2;d <= p-1; d++)
+		{
+			
+			cout << d;
+			if (p%d == 0)
+			{
+				cout << "p ";
+				cout << "Jest pierwsze";
+				lp += 1;
+			}
+			
+			p += 1;
+		}
+		if(p%d!=0)
+		{
+			
+		}
 	}
+		
 }
 
 
