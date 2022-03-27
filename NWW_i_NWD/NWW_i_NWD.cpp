f1bb2b74a05cc0ab55321edd301fd2f2e7e9b4fe
Mikolaj Baran <mikolaj.baran-1@wp.pl>
2022-03-27 12:49:17 +0100

    Added the NWW and NWD script with its timer + updated readme

---
+++ NWW_i_NWD/NWW_i_NWD.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+int main()
+{
+    auto start = high_resolution_clock::now();
+    int a = 0;
+    int b = 0;
+    cout << "Hello World!\n";
+    cout << "Podaj dwie liczby\n";
+    cin >> a >> b;
+    int iloczyn = a * b;
+    while (a != b)
+    {
+        if (a <= b)
+        {
+            b = b - a;
+        }
+        else
+        {
+            a = a - b;
+        }
+    }
+    float nww = iloczyn / a;
+    cout << "NWD = " << a << endl;
+    cout << "NWW = " << nww;
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "\n Ilosc milisekund do wykonania zadania - ";
+    cout << duration.count() << endl;
+}
---
+++ NWW_i_NWD/README.md
@@ -0,0 +1,3 @@
+## Opis
+### NWW i NWD
+Program pyta użytkownika o dwie liczby a następnie wylicza ich największy wspólny dzielnik oraz najmniejszą wspólną wielokrotność. Program najpierw wyszukuje NWD liczb za pomocą Algorytmu Euklidesa, następnie dzieli iloczyn liczb przez NWD aby otrzymać NWW.
