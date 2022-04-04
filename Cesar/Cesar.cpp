34370a6dac94364972a4354bd77e4258ab0879c9
Mikołaj Baran (Uczeń) <mbaran343@technikumkreatywne.pl>
2022-04-04 14:59:46 +0200

    prawie dziala

--- Cesar/Cesar.cpp
+++ Cesar/Cesar.cpp
@@ -2,37 +2,54 @@
 #include <iostream>
 using namespace std;
 void Funkcja(int klucz, char tab[]);
-int main()
-{
-	string slowo;
-	int klucz;
-	char tab[100];
-	cout << "Podaj klucz szyfrowania" << endl;
-	cin >> klucz;
-	cout << "Podaj slowo do zaszyfrowania" << endl;
-	cin >> tab;
-	void funkcja();
-
-}
 void Funkcja(int klucz, char tab[])
 {
 	int dl;
-	int i;
 	dl = strlen(tab);
 	if ( !(klucz >= -26 && klucz <= 26) )
 	{
 		cout << "Blad zly klucz";
 		
 	}
-	else if (klucz <= 26)
+	if (klucz >= 0)
 	{
-		cout << "Blad zly klucz";
-	}
-	else if (klucz >= 0)
-	{
-		while (i < dl)
+		for(int i = 0; i < dl; i++)
 		{
-
+			if (tab[i] + klucz <= 'z')
+			{
+				tab += klucz;
+			}
+			else
+			{
+				tab = tab + klucz - 26;
+			}
 		}
 	}
+	else
+	{
+		for (int i = 0; i < dl; i++)
+		{
+			if (tab[i] + klucz >= 'a')
+			{
+				tab += klucz;
+			}
+			else
+			{
+				tab = tab + klucz + 26;
+			}
+		}
+	}
+	cout << "Po zaszyfrowaniu " << tab << endl;
+}
+int main()
+{
+	string slowo;
+	int klucz;
+	char tab[100];
+	cout << "Podaj wyraz skladajacy sie z malych liter" << endl;
+	cin >> tab;
+	cout << "Podaj klucz z przedzialu [-26..26]" << endl;
+	cin >> klucz;
+	Funkcja(klucz, tab);
+
 }
