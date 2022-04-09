140b4d26a6a37cba4db9c4959ae56d404b3c1153
Mikolaj Baran <mikolaj.baran-1@wp.pl>
2022-04-09 16:14:01 +0200

    finished cesar code

--- Cesar/Cesar.cpp
+++ Cesar/Cesar.cpp
@@ -1,8 +1,10 @@
 ﻿
 #include <iostream>
+#include <chrono>
+using namespace std::chrono;
 using namespace std;
-void Funkcja(int klucz, char tab[]);
-void Funkcja(int klucz, char tab[])
+void Funkcja(int klucz, char tab[], string slowo);
+void Funkcja(int klucz, char tab[], string slowo)
 {
 	int dl;
 	dl = strlen(tab);
@@ -39,6 +41,7 @@ void Funkcja(int klucz, char tab[])
 			}
 		}
 	}
+	cout << "Przed zaszyfrowaniem " << slowo << endl;
 	cout << "Po zaszyfrowaniu " << tab << endl;
 }
 int main()
@@ -46,10 +49,16 @@ int main()
 	string slowo;
 	int klucz;
 	char tab[100];
+	auto start = high_resolution_clock::now();
 	cout << "Podaj wyraz skladajacy sie z malych liter" << endl;
 	cin >> tab;
+	slowo = tab;
 	cout << "Podaj klucz z przedzialu [-26..26]" << endl;
 	cin >> klucz;
-	Funkcja(klucz, tab);
+	Funkcja(klucz, tab, slowo);
 
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop - start);
+	cout << "\n Ilosc milisekund do wykonania zadania - ";
+	cout << duration.count() << endl;
 }
