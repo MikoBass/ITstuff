731ee6cd62a928d78244b0d97410cdb178f21e1e
Mikołaj Baran (Uczeń) <mbaran343@technikumkreatywne.pl>
2022-04-11 14:46:34 +0200

    added a fibbonaci sequence code

---
+++ fibbonaci/fibbonaci.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+#include <chrono>
+using namespace std::chrono;
+using namespace std;
+
+int main()
+{
+
+	int a = 0;
+	int b = 1;
+	int c;
+	int n = 3;		
+	int m;
+	auto start = high_resolution_clock::now();
+	cout << "Ile liczb z ciagu wypisac" << endl;
+	cin >> m;
+	cout << endl;
+	cout << a << endl << b << endl;
+	while (n <= m)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		n++;
+	
+		cout << c << endl;
+	}
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop - start);
+	cout << "\n Ilosc milisekund do wykonania zadania - ";
+	cout << duration.count() << endl;
+}
